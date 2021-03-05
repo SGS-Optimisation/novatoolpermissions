64a0153701a9f984fa77205ee9af2d9b85e3b2eb
Yann Labouy <yann@sugarsplashes.com>
2021-03-05 14:04:36 +0400

    better filtering to cater for multiple terms

--- app/Services/Rule/RuleFilter.php
+++ app/Services/Rule/RuleFilter.php
@@ -6,7 +6,9 @@ namespace App\Services\Rule;
 
 use App\Models\ClientAccount;
 use App\Models\Job;
+use App\Models\Term;
 use App\Services\MySgs\Mapping\Mapper;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 
 class RuleFilter
@@ -22,14 +24,21 @@ class RuleFilter
         $clientRules = [];
 
         if($job->metadata->basicDetails) {
-            $client = ClientAccount::whereRaw('LOWER(alias) LIKE "%' . Str::lower($job->metadata->basicDetails->retailer->customerName) . '%"')->first();
+            $client = ClientAccount::with('rules.terms.taxonomy')->whereRaw('LOWER(alias) LIKE "%' . Str::lower($job->metadata->basicDetails->retailer->customerName) . '%"')->first();
 
             foreach ($client->rules as $rule) {
                 $matched = true;
+                $matchedTaxonomies = [];
+
+                /** @var Term $term */
                 foreach ($rule->terms as $term) {
 
-                    if (Str::lower($term->name) === 'any' || Str::lower($term->name) === 'all') {
+                    if (!Arr::exists($matchedTaxonomies, $term->taxonomy->name)) {
+                        $matchedTaxonomies[$term->taxonomy->name] = false;
+                    }
 
+                    if (Str::lower($term->name) === 'any' || Str::lower($term->name) === 'all') {
+                        $matchedTaxonomies[$term->taxonomy->name] = true;
                         continue;
                     }
 
@@ -48,12 +57,18 @@ class RuleFilter
                          */
                         if (! (Str::contains($termValue, $mysgsValue) || Str::contains($mysgsValue, $termValue)) ) {
                             $matched = false;
+                        } else {
+                            $matchedTaxonomies[$term->taxonomy->name] = true;
                         }
                     }
-
                 }
 
-                if ($matched) {
+                $taxonomyMatch = true;
+                foreach($matchedTaxonomies as $taxonomy => $state) {
+                    $taxonomyMatch &= $state;
+                }
+
+                if ($matched || $taxonomyMatch) {
                     $clientRules[] = $rule;
                 }
             }
