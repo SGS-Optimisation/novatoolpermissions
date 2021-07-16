67733c33d41550b303de63ef34650f14f013835d
Yann Labour <yann@sugarsplashes.com>
2021-07-16 14:38:00 +0400

    disable pruning until proper fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         $schedule->call(function () {
-            (new PruneStaleAttachments)();
+            //(new PruneStaleAttachments)();
         })->daily();
 
         $schedule->command('cache:clear')->lastDayOfMonth();
