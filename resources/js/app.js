c47d8090aa7519655f4d20c135c99f589becaa6b
Yann Labour <yann@sugarsplashes.com>
2021-09-27 23:56:08 +0400

    wip upload component
    todo: stop upload causes error

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,41 +1,23 @@
 require('./bootstrap');
 
-require('moment');
-
-
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/inertia-vue3';
 import { InertiaProgress } from '@inertiajs/progress';
 
-import moment from "moment";
-import Multiselect from 'vue-multiselect';
-import DateFilter from './filters/date' // Import date
-
-const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Dagobah';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => require(`./Pages/${name}.vue`),
     setup({ el, app, props, plugin }) {
-        const vueapp = createApp({ render: () => h(app, props) })
+
+        let vue_app = createApp({ render: () => h(app, props) })
             .use(plugin)
             .mixin({ methods: { route } })
             .mount(el);
 
-        return vueapp;
+        return vue_app;
     },
 });
 
 InertiaProgress.init({ color: '#4B5563' });
-
-//const app = document.getElementById('app');
-
-/*new Vue({
-    render: (h) =>
-        h(InertiaApp, {
-            props: {
-                initialPage: JSON.parse(app.dataset.page),
-                resolveComponent: (name) => require(`./Pages/${name}`).default,
-            },
-        }),
-}).$mount(app);*/
