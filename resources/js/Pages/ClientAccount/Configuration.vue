533e11fe3bd231d842556490923480712bb42995
Yann Labour <yann@sugarsplashes.com>
2021-09-30 21:52:25 +0400

    fix tabs

--- resources/js/Pages/ClientAccount/Configuration.vue
+++ resources/js/Pages/ClientAccount/Configuration.vue
@@ -7,20 +7,17 @@
             <div class="py-12">
                 <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg" v-if="clientAccount != null">
-                        <tabs>
-                            <tab
-                                v-for="(topTaxonomy, index) in topTaxonomies"
-                                :key="topTaxonomy.name"
-                                :name="topTaxonomy.name"
-                                :selected="index == 0"
-                            >
+                        <TabView v-model:activeIndex="activeTab">
+                            <TabPanel v-for="(topTaxonomy, index) in topTaxonomies" :key="topTaxonomy.name" :header="topTaxonomy.name">
+
                                 <child-taxonomies-section
                                     :parent-taxonomy="topTaxonomy"
                                     :taxonomy-hierarchy="taxonomyHierarchy"
                                     :client-account="clientAccount">
                                 </child-taxonomies-section>
-                            </tab>
-                        </tabs>
+
+                            </TabPanel>
+                        </TabView>
 
                     </div>
                 </div>
@@ -40,9 +37,9 @@ import JetInputError from '@/Jetstream/InputError'
 import JetLabel from '@/Jetstream/Label'
 import JetActionMessage from '@/Jetstream/ActionMessage'
 import JetSecondaryButton from '@/Jetstream/SecondaryButton'
-import Tabs from "@/Components/Tabs";
-import Tab from "@/Components/Tab";
 import ChildTaxonomiesSection from "@/Components/PM/ClientAccount/ChildTaxonomiesSection";
+import TabView from 'primevue/tabview/sfc';
+import TabPanel from 'primevue/tabpanel/sfc';
 
 export default {
 
@@ -62,9 +59,15 @@ export default {
         JetInput,
         JetInputError,
         JetLabel,
-        Tabs,
-        Tab,
-        ChildTaxonomiesSection
+        ChildTaxonomiesSection,
+        TabView,
+        TabPanel
+    },
+
+    data() {
+        return {
+            activeTab: 0,
+        }
     },
 }
 </script>
