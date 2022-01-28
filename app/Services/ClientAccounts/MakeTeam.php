3a80dfe2c3d99e58d242f0d5e1001cb1ae6dc7d5
Yann Labour <yann@sugarsplashes.com>
2022-01-28 18:40:24 +0400

    auto adds team leader role to team owners

--- app/Services/ClientAccounts/MakeTeam.php
+++ app/Services/ClientAccounts/MakeTeam.php
@@ -8,7 +8,7 @@ use App\Models\User;
 
 class MakeTeam extends BaseClientAccountService
 {
-    
+
     public function handle($team_owner = null)
     {
         if($this->clientAccount->team) {
@@ -21,6 +21,10 @@ class MakeTeam extends BaseClientAccountService
 
         if(!$team_owner) {
             $team_owner = (auth()->guest() ? User::first() : auth()->user());
+
+            if (!in_array('team-leader', $team_owner->roles->pluck('slug')->all())) {
+                $team_owner->assignRole('team-leader');
+            }
         }
 
         \Log::debug('making new team with owner ' . $team_owner->name);
